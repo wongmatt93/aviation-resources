22fedbcfeecd657be9c8b608653c3e983f51e4f0
lex@lithe.technology <alexagrim95@gmail.com>
2022-12-03 15:12:19 -0800

    styled user page, landing page, new lesson, new test, lesson card & test card

--- src/components/Tests/TestsContainer.tsx
+++ src/components/Tests/TestsContainer.tsx
@@ -1,6 +1,4 @@
-import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
-import { GET_TESTS } from "../../GraphQL/Queries";
 import AppUser from "../../Models/AppUser";
 import { Test } from "../../Models/Test";
 import TestCard from "./TestCard";
@@ -8,21 +6,10 @@ import "./TestsContainer.css";
 
 interface Props {
   user: AppUser | null;
+  tests: Test[];
 }
 
-const TestsContainer = ({ user }: Props) => {
-  const { error, loading, data } = useQuery(GET_TESTS, {
-    variables: { id: user && user.id },
-  });
-  const [tests, setTests] = useState<Test[]>([]);
-
-  useEffect(() => {
-    data && setTests(data.test);
-  }, [data]);
-
-  if (loading) return <p>"Loading..."</p>;
-  if (error) return <p>`Error! ${error.message}`</p>;
-
+const TestsContainer = ({ user, tests }: Props) => {
   return (
     <ul className="TestsContainer">
       {tests.map((test) => (
--- src/components/User/UserProfilePage.tsx
+++ src/components/User/UserProfilePage.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { Image } from "semantic-ui-react";
 import AuthContext from "../../Context/AuthContext";
 import "./UserProfilePage.css";
+import profilePicture from "../../assets/profile-img.png";
 
 const UserProfilePage = () => {
   const { user, signOut } = useContext(AuthContext);
@@ -15,17 +17,24 @@ const UserProfilePage = () => {
   return (
     <main className="UserProfilePage">
       {user && (
-        <>
-          <h2>Profile</h2>
-          <h3>{user.email}</h3>
-          <p>{user.name}</p>
-          <p>Purpose</p>
-          <p>{user.purpose ? user.purpose : "Tap to Select"}</p>
-          <p>Highest License</p>
+        <div className="user-profile">
+          <h2 className="title">Profile</h2>
+          <div className="user-img-and-email">
+            <Image src={profilePicture} />
+            <h3 className="email">E-mail: {user.email}</h3>
+          </div>
+          <p className="user-name"> {user.name ? user.name : "name"} </p>
+          <p>Purpose:</p>
+          <p className="purpose-choices">
+            {user.purpose ? user.purpose : "Tap to Select"}
+          </p>
+          <p className="license">Highest License:</p>
           <p>{user.highest_acs ? user.highest_acs : "Tap to Select"}</p>
-          <button onClick={handleClick}>Log Out</button>
-          <button>Delete Account</button>
-        </>
+          <div className="logout-delete-buttons">
+            <button onClick={handleClick}>Log Out</button>
+            <button>Delete Account</button>
+          </div>
+        </div>
       )}
     </main>
   );
