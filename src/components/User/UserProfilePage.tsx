35b0de3a1111135b97bf74a053f6faa34dbf1d9a
lex@lithe.technology <alexagrim95@gmail.com>
2022-12-27 13:13:35 -0800

    added functionality to input user innfo on profile page

--- src/components/User/UserProfilePage.tsx
+++ src/components/User/UserProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../Context/AuthContext";
 import "./UserProfilePage.css";
@@ -6,12 +6,52 @@ import { FaUserCircle } from "react-icons/fa";
 import Pool from "../../UserPool";
 import { CognitoUserSession } from "amazon-cognito-identity-js";
 import { useMutation } from "@apollo/client";
-import { DELETE_APP_USER } from "../../GraphQL/Mutations";
+import { UPDATE_APP_USER_INFO, DELETE_APP_USER } from "../../GraphQL/Mutations";
 
 const UserProfilePage = () => {
   const { user, signOut } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [purpose, setPurpose] = useState("");
+  const [highestAcs, setHighestAcs] = useState("");
+  const [updateUserInfo] = useMutation(UPDATE_APP_USER_INFO);
   const [deleteUser] = useMutation(DELETE_APP_USER);
   const navigate = useNavigate();
+
+  const handleUpdateName = (name: string) => {
+    setName(name);
+    updateUserInfo({
+      variables: {
+        _set: {
+          name,
+        },
+        where: { id: { _eq: user!.id } },
+      },
+    });
+  };
+
+  const handleUpdatePurpose = (purpose: string) => {
+    setPurpose(purpose);
+    updateUserInfo({
+      variables: {
+        _set: {
+          purpose,
+        },
+        where: { id: { _eq: user!.id } },
+      },
+    });
+  };
+
+  const handleUpdateHighestAcs = (highestAcs: string) => {
+    setHighestAcs(highestAcs);
+    updateUserInfo({
+      variables: {
+        _set: {
+          highest_acs: highestAcs,
+        },
+        where: { id: { _eq: user!.id } },
+      },
+    });
+  };
 
   const deleteAccount = async (email: string): Promise<unknown> => {
     return await new Promise((resolve, reject) => {
@@ -40,8 +80,15 @@ const UserProfilePage = () => {
   };
 
   useEffect(() => {
-    if (user?.email === "guest@aviationresources.io") {
-      navigate("/");
+    if (user) {
+      user.name ? setName(user.name) : setName("name");
+      user.purpose ? setPurpose(user.purpose) : setName("Tap to Select");
+      user.highest_acs
+        ? setHighestAcs(user.highest_acs)
+        : setName("Tap to Select");
+      if (user.email === "guest@aviationresources.io") {
+        navigate("/");
+      }
     }
   }, [user, navigate]);
 
@@ -55,16 +102,45 @@ const UserProfilePage = () => {
               <FaUserCircle className="user-circle" />
               <h3 className="email">E-mail: {user.email}</h3>
             </div>
-            <p className="user-name"> {user.name ? user.name : "name"} </p>
+            <input
+              type="text"
+              className="user-name"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(e) => handleUpdateName(e.target.value)}
+            />
             <div className="purpose-section">
               <p className="section-label">Purpose:</p>
-              <p className="purpose-choices">
-                {user.purpose ? user.purpose : "Tap to Select"}
-              </p>
+              <select
+                name="purpose"
+                id="purpose"
+                value={purpose}
+                onChange={(e) => handleUpdatePurpose(e.target.value)}
+              >
+                <option value="Hobby">Hobby</option>
+                <option value="Student pilot">Student Pilot</option>
+                <option value="Licensed Pilot">Licensed Pilot</option>
+                <option value="Certificated Flight Instructor (CFI)">
+                  Certified Flight Instructor (CFI)
+                </option>
+                <option value="Other">Other</option>
+              </select>
             </div>
             <div className="license-section">
               <p className="section-label">Highest License:</p>
-              <p>{user.highest_acs ? user.highest_acs : "Tap to Select"}</p>
+              <select name="highestAcs" id="highestAcs" value={highestAcs} onChange={e=> handleUpdateHighestAcs(e.target.value)}>
+                <option value="ATP">
+                  Airline Transport Pilot and Type Rating for Airplane
+                </option>
+                <option value="COMM">Commercial Pilot - Airplane</option>
+                <option value="IFR">Instrument Rating - Airplane</option>
+                <option value="PPL">Private Pilot - Airplane</option>
+                <option value="SUAS">
+                  Remote Pilot - Small Unmanned Aircraft Systems (Certiciation
+                  and Recurrent Knowledge Testing)
+                </option>
+              </select>
             </div>
           </div>
           <div className="logout-delete-buttons">
